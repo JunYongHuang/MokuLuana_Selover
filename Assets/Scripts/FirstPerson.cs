05056183720e12dd9d7c2e43ca6f02fdb4661b3f
Eric Selover <eric.selover@gmail.com>
2015-11-05 15:55:22 -0500

    Updated Files
    Added the current version of the game

---
+++ Assets/Scripts/FirstPerson.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPerson : MonoBehaviour {
+
+	Camera cam;
+	CharacterController cc;
+	public float moveSpeed = 100;
+	Vector3 mousePrev;
+	float mouseSensitivityX = .5f;
+	float mouseSensitivityY = .5f;
+	float rotationX = 0;
+	float rotationY = 0;
+
+	void Start () {
+		cam = Camera.main;
+		cc = GetComponent<CharacterController> ();
+	}
+
+	void Update () {
+
+		Vector3 mouseDelta = Input.mousePosition - mousePrev;
+		mousePrev = Input.mousePosition;
+
+		rotationX -= mouseDelta.y * mouseSensitivityY;		
+		rotationY += mouseDelta.x * mouseSensitivityX;
+
+		rotationX = Mathf.Clamp (rotationX, -120, 120);
+		
+		transform.rotation = Quaternion.Euler(new Vector3 (0, rotationY, 0));
+		cam.transform.localRotation = Quaternion.Euler (new Vector3 (rotationX, 0, 0));
+
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		
+
+		Vector3 moveForward = v * transform.forward; 
+		Vector3 moveSideway = h * transform.right;
+
+		Vector3 move = moveForward + moveSideway;
+		if (move.magnitude > 1)	move = move.normalized;
+
+	
+		cc.SimpleMove (move * moveSpeed);
+	}
+}
