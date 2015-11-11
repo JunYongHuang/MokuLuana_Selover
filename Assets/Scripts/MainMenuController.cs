bb403fa0a15118b28317dd1acbc0b9d2865eea8d
Jonathan Champion <champij5@ferris.edu>
2015-11-11 00:00:46 -0500

    Main Menu?

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+	
+	public void PlayGame(){
+		Application.LoadLevel ("IslandMain");
+	}
+	public void ExitGame(){
+		Application.Quit ();
+	}
+	
+}
